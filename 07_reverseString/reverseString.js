d79eb8c78a95acdba676974085130d904e63f66e
pfacanha <pfacanha.dev@gmail.com>
2025-07-18 05:27:57 -0700

    Finish 06-javascript-exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let newStr = '';
+  const length = str.length;
+  for(let i = length - 1; i >= 0; --i){
+    newStr += str[i];
+  }
+  return newStr;
 };
 
 // Do not edit below this line
