e080e13772c111e5dd48ed703af3691ffd4962b2
pfacanha <pfacanha.dev@gmail.com>
2025-08-08 22:03:29 -0700

    Finish 12_calculator javascript-exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  arr.forEach((item) => (sum += item));
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let total = arr[0];
+  for (let i = 1; i < arr.length; ++i) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let temp = a;
+  for (let i = 1; i < b; ++i) {
+    temp *= a;
+  }
+  return temp;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a == 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
