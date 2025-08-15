8a1738b63b1c5c5900314db5c33bcec8c0578704
pfacanha <pfacanha.dev@gmail.com>
2025-08-15 12:21:57 -0700

    Finish 12-palindromes javascript exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  // racecar
+  let wordOne = "";
+  let wordTwo = "";
+  for (let i = 0; i < word.length; ++i) {
+    if (isValid(word[i])) {
+      wordOne += word[i];
+    }
+  }
+  for (let i = word.length - 1; i >= 0; --i) {
+    if (isValid(word[i])) {
+      wordTwo += word[i];
+    }
+  }
 
+  return wordOne.toLowerCase() === wordTwo.toLowerCase();
 };
-
+function isValid(char) {
+  return /[a-zA-Z0-9]/.test(char);
+}
 // Do not edit below this line
 module.exports = palindromes;
