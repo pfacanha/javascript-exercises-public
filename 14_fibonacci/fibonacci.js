d3f0a5b285b6b84e807ab6e0f903a8e4f2387b09
pfacanha <pfacanha.dev@gmail.com>
2025-08-16 10:53:43 -0700

    Finish 14-fibonacci with a naive solution

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = Number(num);
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else if (num > 1) {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
